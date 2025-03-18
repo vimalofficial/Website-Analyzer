b42e3b8e55fce515a11c7b4e5862a9c9279566c4
vimalofficial <vimal7pk@gmail.com>
2025-03-18 16:44:45 +0530

    changedvml

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ import * as chromeLauncher from 'chrome-launcher';
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
@@ -519,3 +519,4 @@ app.delete("/delete/:id", async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
